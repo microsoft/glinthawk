b06690fcddd6cd180b82dc0d434c4734e11feb7c
Sadjad Fouladi <sfouladi@gmail.com>
2023-07-11 20:38:31 -0700

    Initial commit

---
+++ etc/cflags.cmake
@@ -0,0 +1,10 @@
+set ( CMAKE_CXX_STANDARD 20 )
+set ( CMAKE_CXX_STANDARD_REQUIRED ON )
+set ( CMAKE_CXX_EXTENSIONS OFF )
+set ( CMAKE_EXPORT_COMPILE_COMMANDS ON )
+
+if ( MSVC )
+    add_compile_options ( /W4 /WX )
+else ()
+    add_compile_options ( -pedantic -pedantic-errors -Werror -Wall -Wextra -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2 -Weffc++ -Wold-style-cast )
+endif ()
