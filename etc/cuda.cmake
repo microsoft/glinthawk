ea1dfbc6ecdd81cc41f18f3c1cad0ec5e0784395
Sadjad Fouladi <sfouladi@microsoft.com>
2023-08-15 09:19:33 -0700

    Minor fixes to the CUDA build.

--- etc/cuda.cmake
+++ etc/cuda.cmake
@@ -1,11 +1,14 @@
 include ( CheckLanguage )
 check_language ( CUDA )
 
+cmake_policy ( SET CMP0074 NEW )
+find_package ( CUDAToolkit REQUIRED )
+
 if ( CMAKE_CUDA_COMPILER )
   message ( "CUDA compiler found: ${CMAKE_CUDA_COMPILER}" )
   add_definitions ( -DCUDA_ENABLED )
-  set ( CMAKE_CUDA_ARCHITECTURES native )
-  # set ( CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS}" )
+  set ( CMAKE_CUDA_ARCHITECTURES 90 )
+  set ( CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS}" )
   set ( CUDA_ENABLED ON )
-  link_libraries ( -lcublas )
+  link_libraries ( CUDA::cublas )
 endif()
