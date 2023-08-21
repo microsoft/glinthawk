4936986b3e27d21e1c0643335383bfe194c96a5a
Sadjad Fouladi <sfouladi@microsoft.com>
2023-08-21 11:12:56 -0700

    digest.hh: Add boilerplate stuff to SHA256Hash type.

--- src/util/digest.hh
+++ src/util/digest.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstring>
 #include <openssl/sha.h>
 #include <string>
 #include <string_view>
@@ -9,6 +10,19 @@ namespace glinthawk::util::digest {
 struct SHA256Hash
 {
   uint8_t hash[SHA256_DIGEST_LENGTH];
+
+  SHA256Hash() = default;
+  SHA256Hash( const SHA256Hash& other ) { std::memcpy( hash, other.hash, SHA256_DIGEST_LENGTH ); }
+  SHA256Hash& operator=( const SHA256Hash& other )
+  {
+    std::memcpy( hash, other.hash, SHA256_DIGEST_LENGTH );
+    return *this;
+  }
+
+  SHA256Hash( SHA256Hash&& other ) noexcept = default;
+  SHA256Hash& operator=( SHA256Hash&& other ) = default;
+
+  auto operator<=>( const SHA256Hash& other ) const { return std::memcmp( hash, other.hash, SHA256_DIGEST_LENGTH ); }
 };
 
 std::string sha256_base58( std::string_view input );
