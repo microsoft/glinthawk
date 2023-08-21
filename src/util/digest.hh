7850375385aa65b7c1b4b5bec229cb2e396ad3e5
Sadjad Fouladi <sfouladi@microsoft.com>
2023-08-21 11:03:32 -0700

    digest.hh: Add SHA256Hash type.

--- src/util/digest.hh
+++ src/util/digest.hh
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <openssl/sha.h>
 #include <string>
 #include <string_view>
 
 namespace glinthawk::util::digest {
 
+struct SHA256Hash
+{
+  uint8_t hash[SHA256_DIGEST_LENGTH];
+};
+
 std::string sha256_base58( std::string_view input );
+void sha256( const std::string_view input, SHA256Hash& hash );
 
 }
