215c9d21147de1415584d87aa05667cc619ed868
Sadjad Fouladi <sfouladi@microsoft.com>
2023-08-30 10:26:36 -0700

    message/util.cc: Fix a namespace issue.

--- src/message/util.cc
+++ src/message/util.cc
@@ -5,41 +5,41 @@
 using namespace std;
 using namespace glinthawk::core;
 
-string put_field( const bool n ) { return n ? string( 1, '\x01' ) : string( 1, '\x00' ); }
+string glinthawk::core::put_field( const bool n ) { return n ? string( 1, '\x01' ) : string( 1, '\x00' ); }
 
-string put_field( const uint64_t n )
+string glinthawk::core::put_field( const uint64_t n )
 {
   const uint64_t network_order = htobe64( n );
   return string( reinterpret_cast<const char*>( &network_order ), sizeof( network_order ) );
 }
 
-string put_field( const uint32_t n )
+string glinthawk::core::put_field( const uint32_t n )
 {
   const uint32_t network_order = htobe32( n );
   return string( reinterpret_cast<const char*>( &network_order ), sizeof( network_order ) );
 }
 
-string put_field( const uint16_t n )
+string glinthawk::core::put_field( const uint16_t n )
 {
   const uint16_t network_order = htobe16( n );
   return string( reinterpret_cast<const char*>( &network_order ), sizeof( network_order ) );
 }
 
-void put_field( char* message, const bool n, size_t loc ) { message[loc] = n ? '\x01' : '\x00'; }
+void glinthawk::core::put_field( char* message, const bool n, size_t loc ) { message[loc] = n ? '\x01' : '\x00'; }
 
-void put_field( char* message, const uint64_t n, size_t loc )
+void glinthawk::core::put_field( char* message, const uint64_t n, size_t loc )
 {
   const uint64_t network_order = htobe64( n );
   memcpy( message + loc, reinterpret_cast<const char*>( &network_order ), sizeof( network_order ) );
 }
 
-void put_field( char* message, const uint32_t n, size_t loc )
+void glinthawk::core::put_field( char* message, const uint32_t n, size_t loc )
 {
   const uint32_t network_order = htobe32( n );
   memcpy( message + loc, reinterpret_cast<const char*>( &network_order ), sizeof( network_order ) );
 }
 
-void put_field( char* message, const uint16_t n, size_t loc )
+void glinthawk::core::put_field( char* message, const uint16_t n, size_t loc )
 {
   const uint32_t network_order = htobe16( n );
   memcpy( message + loc, reinterpret_cast<const char*>( &network_order ), sizeof( network_order ) );
