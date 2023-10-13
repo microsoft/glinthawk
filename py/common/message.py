44ffcf5daf2161fe4de641fad7978691cd588b36
Sadjad Fouladi <sfouladi@microsoft.com>
2023-10-13 10:53:18 -0700

    common/message.py: Fix a bug in parsing the message header.

--- py/common/message.py
+++ py/common/message.py
@@ -5,6 +5,7 @@ import struct
 
 from typing import Union
 
+
 class Message:
     HEADER_LENGTH = 5
 
@@ -14,8 +15,14 @@ class Message:
         Bye = enum.auto()
         InferenceState = enum.auto()
 
-    def __init__(self, opcode: OpCode, payload: bytes):
-        self.opcode = opcode
+    def __init__(self, opcode: Union[OpCode, int], payload: bytes):
+        if isinstance(opcode, int):
+            self.opcode = Message.OpCode(opcode)
+        elif isinstance(opcode, Message.OpCode):
+            self.opcode = opcode
+        else:
+            raise ValueError("Invalid opcode")
+
         self.payload = payload
 
     def serialize_header(self):
@@ -24,17 +31,18 @@ class Message:
     @staticmethod
     def parse_header(header: bytes) -> [int, int]:
         if len(header) != Message.HEADER_LENGTH:
-            raise ValueError('Invalid header length')
+            raise ValueError("Invalid header length")
 
-        payload_length, opcode = struct.unpack('=BI', header)
+        payload_length, opcode = struct.unpack("!IB", header)
+        print(f"payload_length={payload_length}, opcode={opcode}")
         return [payload_length, opcode]
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return f'Message(opcode={self.opcode}, payload.len={len(self.payload)})'
+        return f"Message(opcode={self.opcode}, payload.len={len(self.payload)})"
 
     def serialize(self) -> bytes:
-        header = struct.pack('=BI', len(self.payload), self.opcode.value)
+        header = struct.pack("!IB", len(self.payload), self.opcode.value)
         return header + self.payload
