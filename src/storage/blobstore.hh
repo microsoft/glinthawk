312a4417508bf3e2c6d327b6c3502bb4e800039a
Sadjad Fouladi <sfouladi@microsoft.com>
2023-10-20 14:52:28 -0700

    blobstore.hh: Refine the interface.

--- src/storage/blobstore.hh
+++ src/storage/blobstore.hh
@@ -6,7 +6,7 @@
 
 namespace glinthawk::storage {
 
-enum class OperationResult
+enum class OpResult
 {
   OK,
   NotFound,
@@ -19,11 +19,13 @@ public:
   BlobStore() {};
   virtual ~BlobStore() {}
 
-  virtual std::pair<OperationResult, std::string> get( const std::string& key ) = 0;
-  virtual OperationResult put( const std::string& key, const std::string& value ) = 0;
+  virtual std::pair<OpResult, std::string> get( const std::string& key ) = 0;
+  virtual OpResult put( const std::string& key, const std::string& value ) = 0;
+  virtual OpResult remove( const std::string& key ) = 0;
 
-  virtual std::vector<std::pair<OperationResult, std::string>> get( const std::vector<std::string>& keys ) = 0;
-  virtual std::vector<OperationResult> put( const std::vector<std::pair<std::string, std::string>>& kvs ) = 0;
+  virtual std::vector<std::pair<OpResult, std::string>> get( const std::vector<std::string>& keys ) = 0;
+  virtual std::vector<OpResult> put( const std::vector<std::pair<std::string, std::string>>& kvs ) = 0;
+  virtual std::vector<OpResult> remove( const std::vector<std::string>& keys ) = 0;
 };
 
 } // namespace glinthawk::storage
