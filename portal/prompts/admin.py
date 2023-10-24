dee439eba453bec22fd2716890216a4171e49753
Sadjad Fouladi <sfouladi@microsoft.com>
2023-10-24 10:45:16 -0700

    portal: Allow blank timestamps for Prompts.

--- portal/prompts/admin.py
+++ portal/prompts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Prompt
+
+class PromptAdmin(admin.ModelAdmin):
+    pass
+
 # Register your models here.
+admin.site.register(Prompt, PromptAdmin)
