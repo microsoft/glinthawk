48a33cacc8b88778075436a0d6664a18c00b9145
Sadjad Fouladi <sfouladi@microsoft.com>
2023-10-24 12:51:12 -0700

    portal: Create a view for submitting prompts.

--- portal/portal/urls.py
+++ portal/portal/urls.py
@@ -18,13 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
-from prompts import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+import prompts.views
 
 router = routers.DefaultRouter()
-router.register(r'prompts', views.PromptViewSet)
+router.register(r'prompts', prompts.views.PromptViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("submit/", prompts.views.SubmitPromptView.as_view()),
     path("api-auth/", include('rest_framework.urls', namespace='rest_framework')),
 ]
--- portal/prompts/serializers.py
+++ portal/prompts/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+import base58
+
 from .models import Prompt
 
 class PromptSerializer(serializers.ModelSerializer):
@@ -8,3 +10,20 @@ class PromptSerializer(serializers.ModelSerializer):
         model = Prompt
         fields = ["hash", "status", "submitted_at", "preprocessed_at", "completed_at"]
         read_only_fields = ["hash", "submitted_at"]
+
+class PromptSubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prompt
+        fields = ["text", "language_model"]
+        read_only_fields = ["hash", "submitted_at"]
+
+    def create(self, validated_data):
+        prompt_text = validated_data["text"]
+        return Prompt.objects.create(
+            user=self.context["request"].user,
+            hash=base58.b58encode(prompt_text).decode(),
+            container="test",
+            origin="test",
+            language_model=validated_data["language_model"],
+            status=1,
+        )
