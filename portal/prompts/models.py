943f48309460906b3a75c5b84bf16ead2cf9b803
Sadjad Fouladi <sfouladi@microsoft.com>
2023-10-24 10:45:16 -0700

    portal: Allow blank timestamps for Prompts.

--- portal/prompts/models.py
+++ portal/prompts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Prompt(models.Model):
     class Status(models.IntegerChoices):
         SUBMITTED = 1, "Submitted"
@@ -14,14 +15,15 @@ class Prompt(models.Model):
         LLAMA2_70B = "llama2_70b", "LLaMa2 70B"
         LLAMA2_70B_CHAT = "llama2_70b_chat", "LLaMa2 70B Chat"
 
-
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     hash = models.CharField(max_length=64)
     container = models.CharField(max_length=256)
     origin = models.CharField(max_length=256, null=True)
-    language_model = models.CharField(max_length=32, choices=LanguageModel.choices)
+    language_model = models.CharField(
+        max_length=32, choices=LanguageModel.choices, default=LanguageModel.LLAMA2_7B
+    )
     status = models.IntegerField(choices=Status.choices, default=Status.SUBMITTED)
     submitted_at = models.DateTimeField(auto_now_add=True)
-    preprocessed_at = models.DateTimeField(null=True)
-    completed_at = models.DateTimeField(null=True)
+    preprocessed_at = models.DateTimeField(null=True, blank=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
