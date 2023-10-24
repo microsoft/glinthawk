22ec34b97c8c7f3c66b8fbdb073ba5371fbdec4a
Sadjad Fouladi <sfouladi@microsoft.com>
2023-10-24 13:54:11 -0700

    prompts/models.py: Create the Job model.

--- portal/prompts/models.py
+++ portal/prompts/models.py
@@ -2,6 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Job(models.Model):
+    class LanguageModel(models.TextChoices):
+        LLAMA2_7B = "llama2_7b", "LLaMa2 7B"
+        LLAMA2_7B_CHAT = "llama2_7b_chat", "LLaMa2 7B Chat"
+        LLAMA2_70B = "llama2_70b", "LLaMa2 70B"
+        LLAMA2_70B_CHAT = "llama2_70b_chat", "LLaMa2 70B Chat"
+
+    language_model = models.CharField(
+        max_length=32, choices=LanguageModel.choices, default=LanguageModel.LLAMA2_7B
+    )
+
+    file = models.FileField(upload_to="raw/", blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
+
+
 class Prompt(models.Model):
     class Status(models.IntegerChoices):
         SUBMITTED = 1, "Submitted"
@@ -9,21 +27,8 @@ class Prompt(models.Model):
         PREPROCESSED = 3, "Preprocessed"
         COMPLETED = 4, "Completed"
 
-    class LanguageModel(models.TextChoices):
-        LLAMA2_7B = "llama2_7b", "LLaMa2 7B"
-        LLAMA2_7B_CHAT = "llama2_7b_chat", "LLaMa2 7B Chat"
-        LLAMA2_70B = "llama2_70b", "LLaMa2 70B"
-        LLAMA2_70B_CHAT = "llama2_70b_chat", "LLaMa2 70B Chat"
-
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, null=True)
     hash = models.CharField(max_length=64)
-    container = models.CharField(max_length=256)
-    origin = models.CharField(max_length=256, null=True)
-    language_model = models.CharField(
-        max_length=32, choices=LanguageModel.choices, default=LanguageModel.LLAMA2_7B
-    )
     status = models.IntegerField(choices=Status.choices, default=Status.SUBMITTED)
-    submitted_at = models.DateTimeField(auto_now_add=True)
     preprocessed_at = models.DateTimeField(null=True, blank=True)
     completed_at = models.DateTimeField(null=True, blank=True)
