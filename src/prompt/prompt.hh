47139f41504fd6f44442b8fba870e9720f11c68d
Sadjad Fouladi <sfouladi@microsoft.com>
2023-10-27 13:05:39 -0700

    Implement CompletionManager.

--- src/prompt/prompt.hh
+++ src/prompt/prompt.hh
@@ -15,12 +15,30 @@ namespace glinthawk::prompt {
 
 class Prompt
 {
-private:
+protected:
   std::vector<uint32_t> tokens_ {};
+
+  Prompt() = default;
 
 public:
   Prompt( const std::string_view serialized_prompt );
   uint32_t token( const uint32_t token_pos ) const { return tokens_.at( token_pos ); }
+};
+
+class Completion : public Prompt
+{
+private:
+  bool is_terminated_ { false };
+
+public:
+  Completion() {}
+
+  void add_token( const uint32_t token ) { tokens_.push_back( token ); }
+  std::string serialize();
+  size_t length() { return tokens_.size(); }
+
+  void terminate() { is_terminated_ = true; }
+  bool is_terminated() { return is_terminated_; }
 };
 
 class PromptManager
@@ -32,8 +50,22 @@ private:
 public:
   PromptManager( std::unique_ptr<storage::BlobStore>&& blobstore );
 
-  std::shared_ptr<Prompt> prompt( const PromptID& prompt_id );
-  void preload_prompts( const std::vector<PromptID>& prompt_ids );
+  std::shared_ptr<Prompt> get( const PromptID& prompt_id );
+  void preload( const std::vector<PromptID>& prompt_ids );
 };
 
-}
+class CompletionManager
+{
+private:
+  std::unique_ptr<storage::BlobStore> blobstore_ {};
+  std::unordered_map<PromptID, std::shared_ptr<Completion>> completions_ {};
+
+public:
+  CompletionManager( std::unique_ptr<storage::BlobStore>&& blobstore );
+
+  std::shared_ptr<Completion> get( const PromptID& prompt_id );
+
+  void commit(); // upload all terminated completions to blobstore
+};
+
+} // namespace glinthawk::prompt
