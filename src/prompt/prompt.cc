00b5e1424a4a31ced97ec26d9c6d0ac02fbcf878
Sadjad Fouladi <sfouladi@microsoft.com>
2023-10-27 13:05:39 -0700

    Implement CompletionManager.

--- src/prompt/prompt.cc
+++ src/prompt/prompt.cc
@@ -44,7 +44,7 @@ PromptManager::PromptManager( unique_ptr<storage::BlobStore>&& blobstore )
 {
 }
 
-shared_ptr<Prompt> PromptManager::prompt( const PromptID& prompt_id )
+shared_ptr<Prompt> PromptManager::get( const PromptID& prompt_id )
 {
   if ( prompts_.count( prompt_id ) == 0 ) {
     LOG( FATAL ) << "Prompt " << prompt_id.base58digest() << " not loaded";
@@ -53,7 +53,7 @@ shared_ptr<Prompt> PromptManager::prompt( const PromptID& prompt_id )
   return prompts_.at( prompt_id );
 }
 
-void PromptManager::preload_prompts( const vector<PromptID>& prompt_ids )
+void PromptManager::preload( const vector<PromptID>& prompt_ids )
 {
   vector<string> keys;
   for ( const auto& prompt_id : prompt_ids ) {
@@ -68,6 +68,56 @@ void PromptManager::preload_prompts( const vector<PromptID>& prompt_ids )
       prompts_[prompt_ids[i]] = make_shared<Prompt>( results[i].second );
     } else {
       LOG( FATAL ) << "Failed to load prompt " << prompt_ids[i].base58digest();
+    }
+  }
+}
+
+string Completion::serialize()
+{
+  CHECK( is_terminated_ ) << "Cannot serialize an incomplete completion";
+
+  string serialized_completion;
+  serialized_completion.reserve( sizeof( uint32_t ) + tokens_.size() * sizeof( uint32_t ) );
+
+  const auto token_count = htole32( tokens_.size() );
+  serialized_completion.append( reinterpret_cast<const char*>( &token_count ), sizeof( token_count ) );
+
+  for ( const auto& token : tokens_ ) {
+    const auto le_token = htole32( token );
+    serialized_completion.append( reinterpret_cast<const char*>( &le_token ), sizeof( le_token ) );
+  }
+
+  return serialized_completion;
+}
+
+shared_ptr<Completion> CompletionManager::get( const PromptID& prompt_id )
+{
+  if ( completions_.count( prompt_id ) == 0 ) {
+    completions_[prompt_id] = make_shared<Completion>();
+  }
+
+  return completions_.at( prompt_id );
+}
+
+void CompletionManager::commit()
+{
+  vector<pair<string, string>> key_values;
+
+  for ( auto it = completions_.begin(); it != completions_.end(); ) {
+    if ( it->second->is_terminated() ) {
+      key_values.emplace_back( "completed/"s + it->first.base58digest() + ".ghc", it->second->serialize() );
+      it = completions_.erase( it );
+    } else {
+      it++;
+    }
+  }
+
+  auto results = blobstore_->put( key_values );
+  CHECK( results.size() == key_values.size() ) << "Failed to commit all completions";
+
+  for ( size_t i = 0; i < results.size(); i++ ) {
+    if ( results[i] != storage::OpResult::OK ) {
+      LOG( FATAL ) << "Failed to commit completion " << key_values[i].first;
     }
   }
 }
