dc309b991fd5b0dba5d0393e7a95bf539c748f14
Pouya Hamadanian <pouyah@mit.edu>
2023-12-04 11:08:38 -0500

    base.cc: is_finished() now compatible with stages

--- src/models/llama2/base.cc
+++ src/models/llama2/base.cc
@@ -316,9 +316,7 @@ void BaseLlama2<Config, DType, Context, StorageDeleter>::dummy_forward( Inferenc
 template<typename Config, typename DType, typename Context, typename StorageDeleter>
 bool BaseLlama2<Config, DType, Context, StorageDeleter>::is_finished( const InferenceState& inference_state )
 {
-  // TODO: rewrite this for split pipes
-  CHECK_EQ( inference_state.next_layer(), settings_.start_layer_num );
-  return ( inference_state.next_layer() == 0 )
+  return ( inference_state.next_layer() == 0 and inference_state.next_stage() == InferenceState::Stage::PreAttention )
          and ( inference_state.token() == 2 or inference_state.token_pos() >= Config::seq_len ); // EOS or out of length
 }
 
