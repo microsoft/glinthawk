dca1a78a24562c24ecd43c1fadc33136c5197ed6
Sadjad Fouladi <sfouladi@microsoft.com>
2024-01-02 10:51:54 -0800

    portal: Remove the Django Rest Framework dependencies.

--- portal/portal/urls.py
+++ portal/portal/urls.py
@@ -1,17 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework import routers
-from rest_framework.urlpatterns import format_suffix_patterns
-
-import prompts.views
-
-router = routers.DefaultRouter()
-router.register(r'jobs', prompts.views.JobViewSet)
-router.register(r'prompts', prompts.views.PromptViewSet)
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(router.urls)),
-    path("api-auth/", include('rest_framework.urls', namespace='rest_framework')),
 ]
