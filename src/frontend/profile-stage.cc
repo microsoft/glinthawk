b2ccf6d36c3411e869f80f9615913f2f174928cc
Sadjad Fouladi <sfouladi@microsoft.com>
2024-01-24 14:59:02 -0800

    profile-stage.cc: Allocate everything first, then run inference.

--- src/frontend/profile-stage.cc
+++ src/frontend/profile-stage.cc
@@ -27,7 +27,7 @@ static void signal_handler( int )
 
 void usage( const char* argv0 )
 {
-  cout << "Usage: " << argv0 << " <model_root> <stage=(pre|att|post)> <batch_size> <repeats>" << endl;
+  cout << "Usage: " << argv0 << " <model_root> <stage=(pre|att|post)> <batch_size> <token_pos> <repeats>" << endl;
 }
 
 int main( int argc, char* argv[] )
@@ -36,7 +36,7 @@ int main( int argc, char* argv[] )
     abort();
   }
 
-  if ( argc != 5 ) {
+  if ( argc != 6 ) {
     usage( argv[0] );
     return EXIT_FAILURE;
   }
@@ -52,7 +52,8 @@ int main( int argc, char* argv[] )
     // const string model_name { argv[2] }; // XXX let's fix the model to Llama2_70B_Chat for now
     const string stage { argv[2] };
     const uint64_t batch_size = atoi( argv[3] );
-    const uint64_t repeats = atoi( argv[4] );
+    const int64_t token_pos = atoi( argv[4] );
+    const uint64_t repeats = atoi( argv[5] );
     const int start_layer = 1;
     const int end_layer = 1;
 
@@ -68,26 +69,35 @@ int main( int argc, char* argv[] )
     models::common::_GLINTHAWK_ARCH_NS_::Operations<_GLINTHAWK_DTYPE_> ops { batch_size };
 #endif
 
+    vector<shared_ptr<ContextType>> contexts( batch_size );
+    vector<vector<models::InferenceState>> all_states( repeats );
+
+    LOG( INFO ) << "Allocating contexts...";
+
+    for ( size_t i = 0; i < batch_size; i++ ) {
+      {
+        GlobalScopeTimer<Timer::Category::MemoryAllocationDevice> _ {};
+        contexts[i] = make_shared<ContextType>( model.settings() );
+      }
+      {
+        GlobalScopeTimer<Timer::Category::MemoryInitializationDevice> _ {};
+        ops.randomize_device_buffer( contexts[i]->layer( start_layer ).token( 0 ).key(),
+                                     contexts[i]->max_size( model.settings().n_layers_loaded() )
+                                       / sizeof( _GLINTHAWK_DTYPE_ ),
+                                     -10.0 / sqrtf( ConfigType::dim ),
+                                     10.0 / sqrtf( ConfigType::dim ) );
+      }
+    }
+
+    LOG( INFO ) << "Create inference states for all repeats...";
+
     for ( size_t r = 0; r < repeats; r++ ) {
-      vector<shared_ptr<ContextType>> contexts( batch_size );
-      vector<models::InferenceState> states( batch_size );
+      auto& states = all_states[r];
+      states.resize( batch_size );
 
-      LOG_EVERY_N( INFO, 100 ) << "Preparing states and contexts for repeat " << r << "...";
       for ( size_t i = 0; i < batch_size; i++ ) {
-        {
-          GlobalScopeTimer<Timer::Category::MemoryAllocationDevice> _ {};
-          contexts[i] = make_shared<ContextType>( model.settings() );
-        }
-        {
-          GlobalScopeTimer<Timer::Category::MemoryInitializationDevice> _ {};
-          ops.randomize_device_buffer( contexts[i]->layer( start_layer ).token( 0 ).key(),
-                                       contexts[i]->max_size( model.settings().n_layers_loaded() )
-                                         / sizeof( _GLINTHAWK_DTYPE_ ),
-                                       -10.0 / sqrtf( ConfigType::dim ),
-                                       10.0 / sqrtf( ConfigType::dim ) );
-        }
-
         DataBuffer state_buffer;
+
         {
           GlobalScopeTimer<Timer::Category::MemoryAllocationHost> _ {};
           state_buffer = DataBuffer { ( 2 * ConfigType::dim + 2 * ConfigType::kv_dim ) * sizeof( _GLINTHAWK_DTYPE_ ) };
@@ -101,37 +111,39 @@ int main( int argc, char* argv[] )
         }
 
         states[i] = { DataType::_GLINTHAWK_DTYPE_NAME_ };
-        states[i].set_token_pos( ConfigType::seq_len - 1 );
+        states[i].set_token_pos( token_pos < 0 ? ConfigType::seq_len - 1 : token_pos );
         states[i].set_activations( move( state_buffer ) );
 
         states[i].set_next_layer( start_layer );
         states[i].set_next_stage( stage == "pre"   ? models::InferenceState::Stage::PreAttention
                                   : stage == "att" ? models::InferenceState::Stage::Attention
                                                    : models::InferenceState::Stage::PostAttention );
+      }
+    }
 
-        string state_serialized;
-        {
-          GlobalScopeTimer<Timer::Category::Serializing> _ {};
-          state_serialized = states[i].serialize();
-        }
-        {
-          GlobalScopeTimer<Timer::Category::Deserializing> _ {};
-          auto state = models::InferenceState { state_serialized };
+    LOG( INFO ) << "Preparation done. Running inference...";
+
+    for ( size_t r = 0; r < repeats; r++ ) {
+      auto& states = all_states[r];
+
+      {
+        GlobalScopeTimer<Timer::Category::PartialInference> _ {};
+
+        if ( stage == "pre" ) {
+          std::ignore = model.pre_attention_forward( std::move( states ), contexts );
+        } else if ( stage == "att" ) {
+          std::ignore = model.attention_forward( std::move( states ), contexts );
+        } else if ( stage == "post" ) {
+          std::ignore = model.post_attention_forward( std::move( states ) );
+        } else {
+          usage( argv[0] );
+          return EXIT_FAILURE;
         }
       }
-      LOG_EVERY_N( INFO, 100 ) << "Preparing states and contexts for repeat " << r << "... done.";
 
-      GlobalScopeTimer<Timer::Category::PartialInference> _ {};
-
-      if ( stage == "pre" ) {
-        std::ignore = model.pre_attention_forward( std::move( states ), contexts );
-      } else if ( stage == "att" ) {
-        std::ignore = model.attention_forward( std::move( states ), contexts );
-      } else if ( stage == "post" ) {
-        std::ignore = model.post_attention_forward( std::move( states ) );
-      } else {
-        usage( argv[0] );
-        return EXIT_FAILURE;
+      if ( r <= 2 && repeats > 3 ) {
+        LOG( INFO ) << "Skipping this repeat for warmup...";
+        global_timer().reset(); // XXX hack to reset the timer
       }
     }
 
