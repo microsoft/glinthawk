53fe84e5b4f8f62882628b3dde918f2f7cc21e8b
Sadjad Fouladi <sfouladi@microsoft.com>
2024-02-23 13:37:27 -0800

    infer-batch.cc: Check for consistency and correct replenishing of free slots.

--- src/frontend/infer-batch.cc
+++ src/frontend/infer-batch.cc
@@ -22,12 +22,24 @@ class BatchInference
 private:
   using StateType = models::BatchedInferenceState<typename Model::ConfigType>;
 
-  const size_t batch_size_;
+  const uint32_t batch_size_;
+  const float temp_;
 
   Model model_;
   models::llama2::Vocabulary vocabulary_;
   StateType state_;
   vector<typename Model::ContextPtr> contexts_ {};
+
+  std::vector<uint32_t> tokens_ { 1 /* BOS */ };
+
+  size_t auto_id_ { 0 };
+
+  PromptID next_prompt_id()
+  {
+    PromptID id;
+    util::digest::sha256( to_string( auto_id_++ ), id );
+    return id;
+  }
 
   StateType ser_des( StateType&& state )
   {
@@ -37,12 +49,23 @@ private:
     return StateType { ser };
   }
 
+  StateType make_state()
+  {
+    StateType st { batch_size_, DataType::_GLINTHAWK_DTYPE_NAME_, {}, {}, false, false, false };
+    for ( size_t i = 0; i < batch_size_; ++i ) {
+      st.set_prompt( i, next_prompt_id(), 1 /* BOS */, 0, temp_, 1 );
+    }
+
+    return st;
+  }
+
 public:
   BatchInference( const filesystem::path& model_path,
                   const filesystem::path& tokenizer_path,
                   const size_t batch_size,
                   const float temp )
     : batch_size_( batch_size )
+    , temp_( temp )
     , model_( model_path, 0, std::numeric_limits<uint32_t>::max(), batch_size, batch_size )
     , vocabulary_( tokenizer_path )
     , state_( batch_size, DataType::_GLINTHAWK_DTYPE_NAME_, {}, {}, false, false, false )
@@ -51,17 +74,18 @@ public:
     state_.set_next_stage( decltype( state_ )::Stage::PreAttention );
 
     for ( size_t i = 0; i < batch_size_; ++i ) {
-      PromptID id;
-      util::digest::sha256( to_string( i ), id );
-
-      state_.set_prompt( i, id, 1 /* BOS */, 0, temp, 1 );
+      state_.set_prompt( i, next_prompt_id(), 1 /* BOS */, 0, temp_, 1 );
       contexts_.push_back( make_shared<typename Model::ContextType>( model_.settings() ) );
     }
   }
 
   void run()
   {
-    for ( size_t i = 0; i < Model::ConfigType::seq_len; i++ ) {
+    std::random_device rd;
+    std::mt19937 gen { rd() };
+    std::uniform_real_distribution<float> dis { 0.0, 1.0 };
+
+    for ( size_t pos = 0; pos < Model::ConfigType::seq_len; pos++ ) {
       for ( size_t layer = 0; layer < Model::ConfigType::n_layers; layer++ ) {
         state_ = ser_des( move( state_ ) );
         state_ = model_.pre_attention_forward( move( state_ ), contexts_ );
@@ -78,7 +102,41 @@ public:
         }
       }
 
-      cout << vocabulary_.get_word( state_.token( 0 ) ) << flush;
+      if ( not temp_ ) {
+        // if temperature is 0, we expect all prompts in the batch to have the same output; the following checks this.
+        for ( size_t i = 0; i < state_.batch_size(); ++i ) {
+          if ( state_.token_pos( i ) < tokens_.size() ) {
+            CHECK_EQ( state_.token( i ), tokens_[state_.token_pos( i )] );
+          } else if ( state_.token_pos( i ) == tokens_.size() ) {
+            tokens_.push_back( state_.token( i ) );
+          } else {
+            LOG( FATAL ) << "Unexpected token pos: " << state_.token_pos( i ) << " vs " << tokens_.size();
+          }
+        }
+
+        // random chance to terminate a prompt early (otherwise they will all have the same length)
+        for ( size_t i = 0; i < state_.batch_size(); ++i ) {
+          if ( state_.token_pos( i ) >= 128 and dis( gen ) < 0.05 ) {
+            state_.set_finished( i );
+          }
+        }
+
+        bool any_finished = false;
+        for ( size_t i = 0; i < state_.batch_size(); ++i ) {
+          if ( state_.finished( i ) ) {
+            state_.discard( i );
+            any_finished = true;
+          }
+        }
+
+        if ( any_finished ) {
+          auto new_state = make_state();
+          state_.replenish_from( new_state );
+          CHECK_EQ( state_.free_slots(), 0 );
+        }
+      }
+
+      cerr << vocabulary_.get_word( state_.token( 0 ) );
     }
   }
 };
