7a0a8656468859d41dd0cc5cb4e9a6b35c0f0761
Sadjad Fouladi <sfouladi@gmail.com>
2024-05-01 10:14:39 -0700

    infer.cc: Write completed prompts to a file, print the status.

--- src/frontend/infer.cc
+++ src/frontend/infer.cc
@@ -38,18 +38,13 @@ private:
   vector<typename Model::ContextPtr> active_contexts_ {};
   vector<prompt::Prompt> finished_prompts_ {};
 
-  StateType ser_des( StateType&& state )
-  {
-    const std::string ser = state.serialize();
-    state = {};
-
-    return StateType { ser };
-  }
+  ofstream completions_file_;
 
 public:
   BatchInference( const filesystem::path& model_path,
                   const filesystem::path& tokenizer_path,
                   const filesystem::path& prompts_path,
+                  const filesystem::path& completions_path,
                   const size_t batch_size,
                   const float temp )
     : batch_size_( batch_size )
@@ -57,6 +52,7 @@ public:
     , model_( model_path, 0, std::numeric_limits<uint32_t>::max(), batch_size, batch_size )
     , vocabulary_( tokenizer_path )
     , state_( batch_size, DataType::_GLINTHAWK_DTYPE_NAME_, {}, {}, false, false, false )
+    , completions_file_( completions_path, ios::out | ios::trunc )
   {
     ifstream prompts_file { prompts_path }; // JSONL file of prompts
     CHECK( prompts_file.is_open() ) << "Failed to open prompts file: " << prompts_path;
@@ -81,25 +77,43 @@ public:
 
   void run()
   {
-    for ( size_t pos = 0; pos < Model::ConfigType::seq_len; pos++ ) {
+    constexpr auto status_interval = chrono::seconds( 5 );
+    auto last_status_print = chrono::steady_clock::now();
+
+    while ( true ) {
+      /* print the status every `status_interval` seconds */
+      if ( const auto now = chrono::steady_clock::now(); now - last_status_print > status_interval ) {
+        last_status_print = now;
+        LOG( INFO ) << "queued: " << prompt_queue_.size() << " active: " << active_prompts_.size()
+                    << " finished: " << finished_prompts_.size();
+      }
+
       for ( size_t layer = 0; layer < Model::ConfigType::n_layers; layer++ ) {
-        state_ = ser_des( move( state_ ) );
         model_.forward_pre_attention( state_ );
-
-        state_ = ser_des( std::move( state_ ) );
         model_.forward_attention( state_, active_contexts_ );
-
-        state_ = ser_des( std::move( state_ ) );
         model_.forward_post_attention( state_ );
 
         if ( state_.next_stage() == InferenceStage::Classification ) {
-          state_ = ser_des( std::move( state_ ) );
           model_.forward_classify( state_ );
         }
       }
 
+      bool any_active = false;
       for ( size_t i = 0; i < batch_size_; i++ ) {
+        if ( not state_.active( i ) ) {
+          continue;
+        }
+
+        any_active = true;
+
+        if ( state_.token_pos( i ) < state_.prompt_length( i ) ) {
+          state_.set_token( i, active_prompts_[i].prompt().at( state_.token_pos( i ) ) );
+        } else {
+          active_prompts_[i].completion().append( state_.token( i ) );
+        }
+
         if ( state_.finished( i ) ) {
+          completions_file_ << active_prompts_[i].to_json() << endl;
           finished_prompts_.push_back( std::move( active_prompts_[i] ) );
 
           // do we have a prompt in the queue to replace this one?
@@ -109,24 +123,26 @@ public:
 
             auto& entry = active_prompts_[i];
             state_.set_prompt( i, entry.id(), entry.prompt().at( 0 ), 0, temp_, entry.prompt().count() );
+          } else {
+            state_.discard( i );
+            state_.clear_discards();
           }
-        }
-
-        if ( state_.token_pos( i ) < state_.prompt_length( i ) ) {
-          state_.set_token( i, active_prompts_[i].prompt().at( state_.token_pos( i ) ) );
-        } else {
-          active_prompts_[i].completion().append( state_.token( i ) );
         }
       }
 
-      cerr << vocabulary_.get_word( state_.token( 0 ) );
+      if ( not any_active ) {
+        // all prompts are finished
+        break;
+      }
     }
   }
 };
 
 void usage( const char* argv0 )
 {
-  cout << "Usage: " << argv0 << " <model_dir> <model_name> <tokenizer_path> <batch_size> <temperature> <prompts.jsonl>"
+  cout << "Usage: " << argv0
+       << " <model_dir> <model_name> <tokenizer_path> <batch_size> <temperature> <in:prompts.jsonl> "
+          "<out:completions.jsonl>"
        << endl;
 }
 
@@ -136,7 +152,7 @@ int main( int argc, char* argv[] )
     abort();
   }
 
-  if ( argc != 7 ) {
+  if ( argc != 8 ) {
     usage( argv[0] );
     return EXIT_FAILURE;
   }
@@ -152,9 +168,11 @@ int main( int argc, char* argv[] )
     const size_t batch_size = atoi( argv[4] );
     const float temp = atof( argv[5] );
     const filesystem::path prompts_path { argv[6] };
+    const filesystem::path completions_path { argv[7] };
 
     using ModelType = llama2::_GLINTHAWK_ARCH_NS_::Stories_110M<_GLINTHAWK_DTYPE_>;
-    BatchInference<ModelType> inference { model_dir_path, tokenizer_path, prompts_path, batch_size, temp };
+    BatchInference<ModelType> inference(
+      model_dir_path, tokenizer_path, prompts_path, completions_path, batch_size, temp );
     inference.run();
 
     cerr << endl << global_timer().summary() << endl;
