add26a14abbca11994da9f31c828be93f94109a6
Sadjad Fouladi <sfouladi@gmail.com>
2024-05-01 10:27:14 -0700

    postprocess.py: Update to match the functionality from preprocess.py.

--- py/postprocess.py
+++ py/postprocess.py
@@ -2,68 +2,59 @@
 
 import os
 import sys
-import struct
 import logging
-import multiprocessing as mp
+import json
+import click
 
 from common.tokenizer import Tokenizer
-from common.serdes import serialize
 
-from rich.logging import RichHandler
+logging.basicConfig(level=logging.INFO)
 
-logging.basicConfig(
-    level=logging.NOTSET, format="%(message)s", datefmt="[%X]", handlers=[RichHandler()]
+
+def postprocess_file(tokenizer, input_file, output_file, discard_tokens=False):
+    with open(input_file, "r") as f_in, open(output_file, "w") as f_out:
+        for line in f_in:
+            entry = json.loads(line)
+            prompt_tokens = entry["prompt"]
+            completion_tokens = entry["completion"]
+            entry["prompt_text"] = "".join(tokenizer.decode(prompt_tokens))
+            entry["completion_text"] = "".join(tokenizer.decode(completion_tokens))
+
+            if discard_tokens:
+                del entry["prompt"]
+                del entry["completion"]
+
+            print(json.dumps(entry), file=f_out, end="\n")
+
+
+@click.command()
+@click.option("--tokenizer-path", required=True, type=click.Path(exists=True))
+@click.option(
+    "--input-file",
+    required=True,
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True),
+    help="A JSONL file containing completed prompts.",
 )
-
-def postprocess_file(tokenizer, input_file, output_dir):
-    output_file = os.path.join(output_dir, os.path.basename(input_file) + ".txt")
-
-    if os.path.exists(output_file):
-        return
-
-    tokens = []
-
-    with open(input_file, "rb") as f:
-        # read first 4 bytes as token count (little-endian)
-        token_count = struct.unpack("<I", f.read(4))[0]
-        for i in range(token_count):
-            token = struct.unpack("<I", f.read(4))[0]
-            tokens += [token]
-
-    tokens = tokenizer.decode(tokens)
-
-    with open(output_file, "w") as f:
-        f.write("".join(tokens))
-
-
-def main(tokenizer_path, input_dir, output_dir):
+@click.option(
+    "--output-file",
+    required=True,
+    type=click.Path(),
+    help="A JSONL file containing the postprocessed prompts.",
+)
+@click.option(
+    "--discard-tokens",
+    is_flag=True,
+    help="Discard the list of tokens from the output.",
+)
+def main(
+    tokenizer_path,
+    input_file,
+    output_file,
+    **kwargs,
+):
     tokenizer = Tokenizer(tokenizer_path)
-    files = [
-        os.path.join(input_dir, x) for x in os.listdir(input_dir) if x.endswith(".ghc")
-    ]
-
-    if len(files) == 0:
-        return
-
-    logging.info("Postprocessing {} files...".format(len(files)))
-
-    os.makedirs(output_dir, exist_ok=True)
-
-    with mp.Pool() as pool:
-        pool.starmap(
-            postprocess_file,
-            [(tokenizer, x, output_dir) for x in files],
-            chunksize=32,
-        )
+    postprocess_file(tokenizer, input_file, output_file, **kwargs)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <tokenizer_path> <input_dir> <output_dir>".format(sys.argv[0]))
-        sys.exit(1)
-
-    tokenizer_path = sys.argv[1]
-    input_dir = sys.argv[2]
-    output_dir = sys.argv[3]
-
-    main(tokenizer_path, input_dir, output_dir)
+    main()
