63c993f4ea9e1843fb5a11443a5c76f97f84edcb
Sadjad Fouladi <sfouladi@microsoft.com>
2024-05-02 13:09:37 -0700

    coordinatory.py: Manage prompts and completions.

--- py/coordinator/coordinator.py
+++ py/coordinator/coordinator.py
@@ -1,14 +1,20 @@
 import settings
 
+import os
+import sys
+import json
 import socket
 import asyncio
 import logging
 
 from enum import Enum
-from rich.logging import RichHandler
 from typing import List, Dict, Tuple
 from dataclasses import dataclass, field
+from signal import SIGINT, SIGTERM
+
+from rich.logging import RichHandler
 from google.protobuf.message import Message as ProtoMessage
+from google.protobuf.json_format import MessageToJson, MessageToDict
 
 from common.message import Message
 from protobuf import glinthawk_pb2 as protobuf
@@ -16,27 +22,33 @@ from protobuf import glinthawk_pb2 as protobuf
 from .worker import Worker
 from .model import Model
 
-logging.basicConfig(level=logging.NOTSET, format="%(message)s", datefmt="[%X]", handlers=[RichHandler()])
-
-
 Platform = protobuf.Hey.Platform
 Stage = protobuf.SetRoute.LayerToAddress.Stage
 
 
 class Coordinator:
     def __init__(self, **kwargs):
-        self.workers: List[Worker] = []
+        # Logging
+        self.logger = logging.getLogger("coordinator")
+        self.logger.setLevel(logging.INFO)
 
-        self.first_worker = None
-        self.classification_worker = None
+        # Workers
+        self.workers: List[Worker] = []
+        self.first_worker = None  # Handle to the first worker in the chain
+        self.classification_worker = None  # Handle to the classification worker
         self.separate_classification_worker = kwargs.get("separate_cls", False)
 
+        # Model
         self.model = Model(
-            name=kwargs.get("model", "stories-110m"),
-            n_layers=kwargs.get("n_layers", 12),
-            layers_per_worker=kwargs.get("layers_per_worker", 6),
+            n_layers=kwargs["n_layers"],
+            layers_per_worker=kwargs["layers_per_worker"],
             separate_cls=self.separate_classification_worker,
         )
+
+        # Job info
+        self.prompt_batch_size = 128
+        self.assigned_prompts = 0
+        self.completed_prompts = 0
 
         # Message queues
         self.incoming_messages = asyncio.Queue()
@@ -47,7 +59,7 @@ class Coordinator:
         self.generated_dummies = 0
         self.completed_dummies = 0
 
-        # Concurrency sizes
+        # Concurrency sizes and context counts
         self.concurrency_size_pre = kwargs.get("concurrency_size_pre", 16)
         self.concurrency_size_att = kwargs.get("concurrency_size_att", 16)
         self.concurrency_size_post = kwargs.get("concurrency_size_post", 16)
@@ -56,14 +68,51 @@ class Coordinator:
         self.cpu_context_count = kwargs.get("cpu_context_count", 36 * 81 * 2)
         self.gpu_context_count = kwargs.get("gpu_context_count", 18 * 81)
 
-        # Logging
-        self.logger = logging.getLogger("coordinator")
-        self.logger.setLevel(logging.INFO)
+        # Prompts and completions
+        self.prompt_queue = []
+        self.completion_queue = asyncio.Queue()
+        self.load_prompts(kwargs.get("prompt_dir"))
+
+        self.output_dir = kwargs.get("output_dir")
+        os.makedirs(self.output_dir, exist_ok=True)
 
     def create_routing_message(self):
         message = self.model.route_message(self.workers)
         message.route_id = 0
         return message
+
+    def load_prompts(self, prompt_dir: str):
+        if not prompt_dir:
+            return
+
+        size_bytes = 0
+
+        for filename in os.listdir(prompt_dir):
+            path = os.path.join(prompt_dir, filename)
+            if filename.endswith(".jsonl") and os.path.isfile(path):
+                with open(path, "r") as f:
+                    for line in f:
+                        size_bytes += len(line)
+                        p = self.make_prompt_from_json(line)
+                        self.prompt_queue.append(p)
+
+        self.logger.info(f"Loaded {len(self.prompt_queue)} prompts ({size_bytes} bytes).")
+
+    def make_prompt_from_json(self, jsondata: str) -> protobuf.Prompt:
+        data = json.loads(jsondata)
+        prompt = protobuf.Prompt()
+
+        if "id" not in data:
+            raise ValueError("Prompt does not have an ID")
+
+        prompt.id = data["id"]
+        prompt.temperature = data.get("temperature", 0)
+        prompt.max_tokens = data.get("max_tokens", 2048)
+        prompt.prompt[:] = data.get("prompt", [1])
+        prompt.completion[:] = []
+        prompt.user_data = data.get("user_data", "")
+
+        return prompt
 
     async def handle_worker(self, reader, writer):
         addr = writer.get_extra_info("peername")
@@ -137,7 +186,7 @@ class Coordinator:
                     worker,
                     Message.OpCode.InitializeWorker,
                     protobuf.InitializeWorker(
-                        model_name=self.model.name,
+                        model_name="model",
                         start_layer=worker.model_slice_start[0],
                         end_layer=worker.model_slice_end[0],
                         concurrency_pre_att_size=worker.concurrency_size_pre,
@@ -165,8 +214,7 @@ class Coordinator:
                             self.push_message(w, Message.OpCode.SetRoute, routing_message)
 
                     self.logger.info(
-                        f"Layer 0 is at {socket.inet_ntoa(self.first_worker.ip)}:{self.first_worker.port}; "
-                        "completions can be found there."
+                        f"The first layer is at {socket.inet_ntoa(self.first_worker.ip)}:{self.first_worker.port}."
                     )
 
                     # Telling the first worker to generate dummy prompts
@@ -184,11 +232,21 @@ class Coordinator:
 
                         # Periodically check if we need to generate more dummy prompts to keep the workers busy
                         asyncio.create_task(self.maybe_generate_dummies())
+                    else:
+                        asyncio.create_task(self.maybe_send_prompts())
 
-            elif message.opcode == Message.OpCode.PromptCompleted:
-                count = int(message.payload.decode())
-                self.logger.info(f"Worker {worker.id} completed {count} prompts.")
-                self.completed_dummies += count
+            elif message.opcode == Message.OpCode.PushCompletions:
+                proto = protobuf.PushCompletions()
+                proto.ParseFromString(message.payload)
+                self.logger.info(f"Worker {worker.id} completed {len(proto.completions)} prompts.")
+
+                if self.initial_dummy_count:
+                    self.completed_dummies += len(proto.completions)
+                else:
+                    self.completed_prompts += len(proto.completions)
+
+                for completion in proto.completions:
+                    self.completion_queue.put_nowait(completion)
 
             else:
                 self.logger.error(f"Unexpected message {message.opcode} from {worker.id}.")
@@ -214,10 +272,39 @@ class Coordinator:
 
             self.generated_dummies += count
 
+    async def maybe_send_prompts(self):
+        while True:
+            if not self.prompt_queue:
+                self.logger.info("All prompts have been submitted for processing.")
+                return
+
+            proto = protobuf.PushPrompts()
+
+            while self.assigned_prompts - self.completed_prompts < self.prompt_batch_size and self.prompt_queue:
+                proto.prompts.append(self.prompt_queue.pop(0))
+                self.assigned_prompts += 1
+
+            if not proto.prompts:
+                continue
+
+            self.logger.info(f"Sending {len(proto.prompts)} prompts to the first worker.")
+            self.push_message(self.first_worker, Message.OpCode.PushPrompts, proto)
+
+            await asyncio.sleep(30)
+
+    async def dump_completions(self):
+        with open(os.path.join(self.output_dir, f"completions.json"), "w") as f:
+            while True:
+                    completion = await self.completion_queue.get()
+                    f.write(json.dumps(MessageToDict(completion), indent=None, separators=(",", ":")))
+                    f.write("\n")
+                    f.flush()
+
     async def main(self, listen_address, listen_port):
         server = await asyncio.start_server(self.handle_worker, listen_address, listen_port)
 
         async with server:
             asyncio.create_task(self.message_processor())
             asyncio.create_task(self.handle_outgoing_messages())
+            asyncio.create_task(self.dump_completions())
             await server.serve_forever()
