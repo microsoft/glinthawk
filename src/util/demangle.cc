ce1b7aa20548422c51f0b0960c2141d8b2f96434
Sadjad Fouladi <sfouladi@microsoft.com>
2024-07-25 11:01:12 -0700

    demangle.cc: Free the buffer returned by `abi::__cxa_demangle`.

--- src/util/demangle.cc
+++ src/util/demangle.cc
@@ -6,15 +6,14 @@
 std::string glinthawk::util::demangle( const std::string& name )
 {
   int status = 0;
-
-  char* buffer = reinterpret_cast<char*>( malloc( name.size() ) );
-  size_t length = name.size();
-
-  std::string demangled = abi::__cxa_demangle( name.c_str(), buffer, &length, &status );
+  char* buffer = abi::__cxa_demangle( name.c_str(), nullptr, nullptr, &status );
 
   if ( status != 0 ) {
     return name;
   }
 
-  return std::string { buffer, length };
+  std::string result { buffer };
+  free( buffer );
+
+  return result;
 }
