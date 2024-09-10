337d0f97df56668569480a7a50148f92db7aa1bb
Pouya Hamadanian <pouyah@mit.edu>
2024-09-10 14:00:28 -0400

    add profiling script and container

--- tools/profile.bash
+++ tools/profile.bash
@@ -1,27 +1,22 @@
-rm $4
-for slices in 1 4
-do
-	for batch in {1..32}
-	do
+#!/bin/bash
 
-		value=$($1 $2 $3 1 $slices $batch 2>&1 | grep "Token Generation" | grep -o 'avg=[[:space:]]*[0-9.]*' | awk -F= '{print $2}')
-		if [ -n "$value" ]; then
-    			echo "Slices: $slices and $batch Batch size: $value" >> "$4"
-		else
-    			echo "Line not found" >> "$4"
-		fi
-	done
-done
-for slices in 31
-do
-        for batch in {1..32}
-        do
+if [ $# -lt 4 ] || [ $# -gt 7 ];  then
+  echo "Usage: $0 <model-dir> <model-name> <context_type=(paged|static)> <stage=(all|all_no_cls|pre|att|post|cls)> [<token_pos=2048> <duration=30> <LOG_DIR=/output_logs/>]"
+  exit 1
+fi
 
-                value=$($1 $2 $3 $slices 31 $batch 2>&1 | grep "Token Generation" | grep -o 'avg=[[:space:]]*[0-9.]*' | awk -F= '{print $2}')
-                if [ -n "$value" ]; then
-                        echo "Slices: $slices and $batch Batch size: $value" >> "$4"
-                else
-                        echo "Line not found" >> "$4"
-                fi
-        done
+MODEL_DIR=$1
+MODEL_NAME=$2
+CTX_TYPE=$3
+STAGE=$4
+TOKEN_POS=${5:-"2047"}
+DURATION=${6:-"30"}
+LOG_DIR=${7:-"/output_logs/"}
+
+batch_size_list=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 18 20 22 24 26 28 30 32 36 40 44 48 52 56 60 64 72 80 88 96 104 112 120 128 144 160 176 192 208 224 240 256 288 320 352 384 416 448 480 512)
+
+for batch_size in "${batch_size_list[@]}"
+do
+    echo "Batch size: $batch_size"
+    /app/profile-stage $MODEL_DIR $MODEL_NAME $CTX_TYPE $STAGE $batch_size $TOKEN_POS $DURATION "${LOG_DIR}/${MODEL_NAME}_${STAGE}_${CTX_TYPE}_${TOKEN_POS}_${DURATION}_${batch_size}.csv"
 done
